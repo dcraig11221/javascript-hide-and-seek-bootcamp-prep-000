c911d0b8e07bc7b308f62bb584cfef33bcfe75ed
Douglas Craig <dcraig112@gmail.com>
2018-04-27 21:45:41 +0000

    Done.

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ function getFirstSelector(selector) {
 function nestedTarget() {
 	return document.querySelector('div.target')
 }
-function increaseRankBy(n) {
-	 let lis = document.querySelectorAll('ul.ranked-list li');
-	  for (let i = 0; i < lis.length; i++) {
-		lis[i].innerHTML = (parseInt(lis[i].innerHTML, 10) + 3).toString()
-		}
- }
- function deepestChild() {
- 	let divs = document.getElementById('grand-node').querySelectorAll('div')
- 	return (divs[divs.length -1])
- }
+
+function deepestChild() {
+	let x = document.getElementById('grand-node').querySelectorAll('div')
+	return x[x.length-1]
+}

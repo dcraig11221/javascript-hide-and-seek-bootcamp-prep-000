e2067b8fe962139f56c72c06d64dfd4584035735
Douglas Craig <dcraig112@gmail.com>
2018-04-27 21:46:19 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function deepestChild() {
 	let x = document.getElementById('grand-node').querySelectorAll('div')
 	return x[x.length-1]
 }
+function increaseRankBy(n) {
+	let x = document.querySelectorAll('.ranked-list li')
+	for (let i = 0; i < x.length; i++) {
+		x[i].innerHTML = parseInt(x[i].innerHTML) + n
+	}
+}
